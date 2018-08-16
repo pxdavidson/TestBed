8d15bdb66a62ad17f2c62a3632c58133670a9e99
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-08-16 16:50:38 +0100

    TEST_01
    ##CODE
    + Tested passing variables between methods.
    REMEMBER TO DEFINE THE VARIABLE IN THE METHOD THAT RECEIVES IT

---
+++ TestBed/Assets/PassingParameterTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassingParameterTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+
+    // Called every frame
+    void Update()
+    {
+	DecideGreeting();
+    }
+
+    // Process user input to determine greeting given
+    void DecideGreeting()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Nice Greeting
+            GreetPlayer("you're lovely");
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Mean Greeting
+            GreetPlayer("fuck you");
+        }
+    }
+
+    void GreetPlayer(string greeting)
+    {
+	    Debug.Log(greeting);
+    }
+}
